0c51187ff90b48646d6780cb0a4809772a64d926
Ryan O'Neill <ryano@twitter.com>
2014-11-22 18:50:35 -0800

    Removed list usage

--- Problem04.scala
+++ Problem04.scala
@@ -7,22 +7,29 @@
 
 object Problem04 {
   def main(args: Array[String]) {
-    val twoDigitNumbers = (10 to 99).toList
-    println("Largest Palindrome Product of 2-Digit Numbers: " + maxPalindromeProduct(twoDigitNumbers))
-
-    val threeDigitNumbers = (100 to 999).toList
-    println("Largest Palindrome Product of 3-Digit Numbers: " + maxPalindromeProduct(threeDigitNumbers))
+    println("Largest Palindrome Product of 2-Digit Numbers: " + maxPalindromeProduct(2))
+    println("Largest Palindrome Product of 3-Digit Numbers: " + maxPalindromeProduct(3))
   }
 
-  def maxPalindromeProduct(numbers: List[Int]) = {
-    val palindromes = 
-      for {
-      	a <- numbers
-      	b <- numbers
-      	value = a * b
-      	if (value.toString == value.toString.reverse)
-      } yield value
-      palindromes.max
+  def isPalindrome(n: Int): Boolean = {
+    val strValue = n.toString
+    strValue == strValue.reverse
   }
+
+  def numsFrom(n: Int): Stream[Int] =
+    n #:: numsFrom(n+1)
+
+  def numsFromTo(m: Int, n: Int): Stream[Int] =
+    numsFrom(m).takeWhile(_ <= n)
+
+  def maxPalindromeProductFromTo(m: Int, n: Int): Int =
+    numsFromTo(m,n).flatMap(x => numsFromTo(m,n).map(_ * x).filter(isPalindrome)).max
+
+  def maxPalindromeProduct(digits: Int): Int = {
+    val lower = math.pow(10,digits-1).toInt
+    val upper = math.pow(10,digits).toInt - 1
+    maxPalindromeProductFromTo(lower,upper)
+  }
+
 }
 
