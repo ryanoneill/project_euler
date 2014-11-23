55ee2ef86b8ea212406964ff0181c8a9f9c7b04e
Ryan O'Neill <ryano@twitter.com>
2014-11-22 17:55:01 -0800

    Fixed dumb fib stream

--- Problem02.scala
+++ Problem02.scala
@@ -13,13 +13,6 @@
 // F1 = 1
 // Fi = Fi-1 + Fi-2
 
-// In the CLRS Algorithms book (page 59), the answer to the recurrence relation is shown as 
-// [Golden Ratio] = (1 + square root 5) / 2
-// [Conjugate] = (1 - square root 5) / 2
-// Fi = ([Golden Ratio]^i - [Conjugate]^i) / (square root 5)
-
-// This problem starts with 1,2 instead of 1,1 so terms are off by 1
-
 object Problem02 {
   def main(args: Array[String]) {
     val limit = 4000000
@@ -27,18 +20,9 @@ object Problem02 {
   }
 
   def evenValuedFibonacciSum(upperBound: Int) = 
-    fibStream(1).takeWhile(_ <= upperBound).filter(_ % 2 == 0).sum
+    fibsFrom(1,2).takeWhile(_ <= upperBound).filter(_ % 2 == 0).sum
 
-  def fibStream(i: Int): Stream[Int] =
-    fibonacci(i) #:: fibStream(i + 1)
+  def fibsFrom(x: Int, y: Int): Stream[Int] =
+    x #:: fibsFrom(y,x+y)
 
-  def fibonacci(i: Int) = {
-    val termIndex = i + 1
-
-    val root5 = math.sqrt(5)
-    val golden = (1 + root5) / 2
-    val conjugate = (1 - root5) / 2
-
-    ((math.pow(golden, termIndex) - math.pow(conjugate, termIndex)) / root5).toInt
-  }
 }
